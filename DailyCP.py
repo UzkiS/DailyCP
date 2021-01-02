b279da7e6467992f35365dea71375129de8f6ec0
xsw0 <xsw.mail.0@gmail.com>
2021-01-02 15:17:48 +0800

    update

--- DailyCP.py
+++ DailyCP.py
@@ -8,84 +8,169 @@ import pyDes
 import base64
 import uuid
 import sys
-import getopt
+import os
+import hashlib
+from Crypto.Cipher import AES
+
 
 class DailyCP:
-    def __init__(self, host="sise"):
-        self.key = "ST83=@XV"#dynamic when app update
-        self.t = str(int(round(time.time() * 1000)))
+    def __init__(self, schoolName="安徽理工大学"):
+        self.key = "ST83=@XV"  # dynamic when app update
         self.session = requests.session()
-        self.host = host + ".campusphere.net"
+        self.host = ""
+        self.loginUrl = ""
+        self.isIAPLogin = True
         self.session.headers.update({
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.122 Safari/537.36",
-            "X-Requested-With": "XMLHttpRequest",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36 Edg/83.0.478.37",
+            # "X-Requested-With": "XMLHttpRequest",
             "Pragma": "no-cache",
             "Accept": "application/json, text/plain, */*",
+            # "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
             # "User-Agent": "okhttp/3.12.4"
         })
-        url = "https://{host}/iap/login?service=https://{host}/portal/login".format(host=self.host)
-        ret = self.session.get(url).url
-        self.client = ret[ret.find("=")+1:]
-        self.session.headers.update({"Content-Type": "application/x-www-form-urlencoded"})
-        url = "https://{host}/iap/security/lt".format(host=self.host)
-        ret = json.loads(self.session.post(url,data="lt={client}".format(client=self.client)).text)
-        self.client = ret["result"]["_lt"]
-        self.encryptSalt = ret["result"]["_encryptSalt"]
+        extension = {"deviceId": str(uuid.uuid4()), "systemName": "未来操作系统", "userId": "5201314",
+                     "appVersion": "8.1.13", "model": "红星一号量子计算机", "lon": 0.0, "systemVersion": "初号机", "lat": 0.0}
+        self.session.headers.update(
+            {"Cpdaily-Extension": self.encrypt(json.dumps(extension))})
+        self.setHostBySchoolName(schoolName)
 
-    def encrypt(self,text):
-        k = pyDes.des(self.key, pyDes.CBC, b"\x01\x02\x03\x04\x05\x06\x07\x08", pad=None, padmode=pyDes.PAD_PKCS5)
+    def setHostBySchoolName(self, schoolName):
+        ret = self.request(
+            "https://static.campushoy.com/apicache/tenantListSort")
+        school = [j for i in ret["data"]
+                  for j in i["datas"] if j["name"] == schoolName]
+        if len(school) == 0:
+            print("不支持的学校或者学校名称错误,以下是支持的学校列表")
+            print(ret)
+            exit()
+        ret = self.request(
+            "https://mobile.campushoy.com/v6/config/guest/tenant/info?ids={ids}".format(ids=school[0]["id"]))
+        self.loginUrl = ret["data"][0]["ampUrl"]
+        if ret == "":
+            print("学校并没有申请入驻今日校园平台")
+            exit()
+        print("{name}的登录地址{url}".format(name=schoolName, url=self.loginUrl))
+        self.host = re.findall(r"//(.*?)/", self.loginUrl)[0]
+
+    def encrypt(self, text):
+        k = pyDes.des(self.key, pyDes.CBC, b"\x01\x02\x03\x04\x05\x06\x07\x08",
+                      pad=None, padmode=pyDes.PAD_PKCS5)
         ret = k.encrypt(text)
         return base64.b64encode(ret).decode()
 
-    def decrypt(self,text):
-        k = pyDes.des(self.key, pyDes.CBC, b"\x01\x02\x03\x04\x05\x06\x07\x08", pad=None, padmode=pyDes.PAD_PKCS5)
+    def passwordEncrypt(self, text: str, key: str):
+        def pad(s): return s + (len(key) - len(s) %
+                                len(key)) * chr(len(key) - len(s) % len(key))
+
+        def unpad(s): return s[:-ord(s[len(s) - 1:])]
+        text = pad(
+            "TdEEGazAXQMBzEAisrYaxRRax5kmnMJnpbKxcE6jxQfWRwP2J78adKYm8WzSkfXJ"+text).encode("utf-8")
+        aes = AES.new(str.encode(key), AES.MODE_CBC,
+                      str.encode("ya8C45aRrBEn8sZH"))
+        return base64.b64encode(aes.encrypt(text))
+
+    def request(self, url: str, body=None, parseJson=True, JsonBody=True, Referer=None):
+        url = url.format(host=self.host)
+        if Referer != None:
+            self.session.headers.update({"Referer": Referer})
+        if body == None:
+            ret = self.session.get(url)
+        else:
+            self.session.headers.update(
+                {"Content-Type": ("application/json" if JsonBody else "application/x-www-form-urlencoded")})
+            ret = self.session.post(url, data=(
+                json.dumps(body) if JsonBody else body))
+        if parseJson:
+            return json.loads(ret.text)
+        else:
+            return ret
+
+    def decrypt(self, text):
+        k = pyDes.des(self.key, pyDes.CBC, b"\x01\x02\x03\x04\x05\x06\x07\x08",
+                      pad=None, padmode=pyDes.PAD_PKCS5)
         ret = k.decrypt(base64.b64decode(text))
         return ret.decode()
 
     def checkNeedCaptcha(self, username):
-        url = "https://{host}/iap/checkNeedCaptcha?username={username}&_=".format(host=self.host,username=username)
+        url = "https://{host}/iap/checkNeedCaptcha?username={username}".format(
+            host=self.host, username=username)
         ret = self.session.get(url)
         ret = json.loads(ret.text)
         return ret["needCaptcha"]
 
     def generateCaptcha(self):
-        url = "https://{host}/iap/generateCaptcha?ltId={client}&codeType=2&".format(host=self.host,client=self.client)
-        ret = self.session.get(url)
-        return ret.content
+        # url = "https://{host}/iap/generateCaptcha?ltId={client}&codeType=2".format(host=self.host,client=self.client)
+        # ret = self.session.get(url)
+        # return ret.content
+        pass
 
     def getBasicInfo(self):
-        url = "https://{host}/iap/tenant/basicInfo".format(host=self.host)
-        ret = self.session.post(url,data="{}").text
-        return json.loads(ret)
+        return self.request("https://{host}/iap/tenant/basicInfo", "{}")
 
     def login(self, username, password, captcha=""):
-        url = "https://{host}/iap/doLogin".format(host=self.host)
-        self.username = username
+        if "campusphere" in self.loginUrl:
+            return self.loginIAP(username, password, captcha)
+        else:
+            return self.loginAuthserver(username, password, captcha)
+
+    def loginIAP(self, username, password, captcha=""):
+        self.session.headers.update({"X-Requested-With": "XMLHttpRequest"})
+
+        ret = self.session.get(
+            "https://{host}/iap/login?service=https://{host}/portal/login".format(host=self.host)).url
+        client = ret[ret.find("=")+1:]
+        ret = self.request("https://{host}/iap/security/lt",
+                           "lt={client}".format(client=client), True, False)
+        client = ret["result"]["_lt"]
+        # self.encryptSalt = ret["result"]["_encryptSalt"]
+
         body = {
             "username": username,
             "password": password,
-            "lt": self.client,
+            "lt": client,
             "captcha": captcha,
             "rememberMe": "true",
             "dllt": "",
             "mobile": ""
         }
-        self.session.headers["Content-Type"] = "application/x-www-form-urlencoded"
-        ret = json.loads(self.session.post(url, data=body).text)
+        ret = self.request("https://{host}/iap/doLogin", body, True, False)
         if ret["resultCode"] == "REDIRECT":
             self.session.get(ret["url"])
             return True
-        else: return False
+        else:
+            return False
+
+    def checkNeedCaptchaAuthServer(self, username):
+        ret = self.request("http://{host}/authserver/needCaptcha.html?username={username}&pwdEncrypt2=pwdEncryptSalt".format(
+            username=username), parseJson=False).text
+        return ret == "true"
+
+    def loginAuthserver(self, username, password, captcha=""):
+        ret = self.request(self.loginUrl, parseJson=False)
+        body = dict(re.findall(
+            r'''<input type="hidden" name="(.*?)" value="(.*?)"''', ret.text))
+        salt = dict(re.findall(
+            r'''<input type="hidden" id="(.*?)" value="(.*?)"''', ret.text))
+        body["username"] = username
+        body["dllt"] = "userNamePasswordLogin"
+        if "pwdDefaultEncryptSalt" in salt.keys():
+            body["password"] = self.passwordEncrypt(
+                password, salt["pwdDefaultEncryptSalt"])
+        else:
+            body["password"] = password
+        ret = self.request(ret.url, body, False, False,
+                           Referer=self.loginUrl).url
+        print(self.session.cookies)
+        print("本函数不一定能用。")
+        return True
 
     def getCollectorList(self):
         body = {
             "pageSize": 10,
             "pageNumber": 1
         }
-        self.session.headers["Content-Type"] = "application/json"
-        url = "https://{host}/wec-counselor-collector-apps/stu/collector/queryCollectorProcessingList".format(host=self.host)
-        ret = self.session.post(url, data=json.dumps(body))
-        ret = json.loads(ret.text)
+        ret = self.request(
+            "https://{host}/wec-counselor-collector-apps/stu/collector/queryCollectorProcessingList", body)
         return ret["datas"]["rows"]
 
     def getNoticeList(self):
@@ -93,152 +178,127 @@ class DailyCP:
             "pageSize": 10,
             "pageNumber": 1
         }
-        self.session.headers["Content-Type"] = "application/json"
-        url = "https://{host}/wec-counselor-stu-apps/stu/notice/queryProcessingNoticeList".format(host=self.host)
-        ret = self.session.post(url, data=json.dumps(body))
-        ret = json.loads(ret.text)
+        ret = self.request(
+            "https://{host}/wec-counselor-stu-apps/stu/notice/queryProcessingNoticeList", body)
         return ret["datas"]["rows"]
 
     def confirmNotice(self, wid):
         body = {
             "wid": wid
         }
-        self.session.headers["Content-Type"] = "application/json"
-        url = "https://{host}/wec-counselor-stu-apps/stu/notice/confirmNotice".format(host=self.host)
-        ret = self.session.post(url, data=json.dumps(body))
-        ret = json.loads(ret.text)
-        if ret["message"] == "SUCCESS":return True
-        else:
-            print(ret["message"])
-            return False
+        ret = self.request(
+            "https://{host}/wec-counselor-stu-apps/stu/notice/confirmNotice", body)
+        print(ret["message"])
+        return ret["message"] == "SUCCESS"
 
     def getCollectorDetail(self, collectorWid):
-        url = "https://{host}/wec-counselor-collector-apps/stu/collector/detailCollector".format(host=self.host)
         body = {
             "collectorWid": collectorWid
         }
-        self.session.headers["Content-Type"] = "application/json"
-        ret = self.session.post(url, data=json.dumps(body))
-        ret = json.loads(ret.text)["datas"]
-        url = ret["form"]["formContent"]
-        return ret
+        return self.request("https://{host}/wec-counselor-collector-apps/stu/collector/detailCollector", body)["datas"]
 
     def getCollectorFormFiled(self, formWid, collectorWid):
-        url = "https://{host}/wec-counselor-collector-apps/stu/collector/getFormFields".format(host=self.host)
         body = {
             "pageSize": 50,
             "pageNumber": 1,
             "formWid": formWid,
             "collectorWid": collectorWid
         }
-        self.session.headers["Content-Type"] = "application/json"
-        ret = self.session.post(url, data=json.dumps(body))
-        ret = json.loads(ret.text)["datas"]["rows"]
-        return ret
+        return self.request("https://{host}/wec-counselor-collector-apps/stu/collector/getFormFields", body)["datas"]["rows"]
 
     def submitCollectorForm(self, formWid, collectWid, schoolTaskWid, rows, address):
-        url = "https://{host}/wec-counselor-collector-apps/stu/collector/submitForm".format(host=self.host)
         body = {
             "formWid": formWid,
             "collectWid": collectWid,
             "schoolTaskWid": schoolTaskWid,
             "form": rows,
-            "address": address
+            "address": address,
+            "uaIsCpadaily": True
         }
-        self.session.headers["Content-Type"] = "application/json"
-        # self.session.headers["extension"] = "1" extension
-        extension = {"deviceId":str(uuid.uuid4()),"systemName":"未来操作系统","userId":self.username,"appVersion":"8.1.13","model":"红星一号量子计算机","lon":0.0,"systemVersion":"初号机","lat":0.0}
-        self.session.headers.update({"Cpdaily-Extension": self.encrypt(json.dumps(extension))})
-        ret = self.session.post(url, data=json.dumps(body))
-        print(ret.text)
-        ret = json.loads(ret.text)
-        if ret["message"] == "SUCCESS":return True
-        else:
-            print(ret["message"])
-            return False
+        ret = self.request(
+            "https://{host}/wec-counselor-collector-apps/stu/collector/submitForm", body)
+        print(ret["message"])
+        return ret["message"] == "SUCCESS"
 
     def autoFill(self, rows):
-        # isRequired
         for item in rows:
             index = 0
             while index < len(item["fieldItems"]):
-                if item["fieldItems"][index]["isSelected"] == 1:index = index + 1
-                else:item["fieldItems"].pop(index)
+                if item["fieldItems"][index]["isSelected"] == 1:
+                    index = index + 1
+                else:
+                    item["fieldItems"].pop(index)
 
-    def autoComplete(self, address):
+    def getFormCharac(self, detail):
+        ret = self.request(detail["content"], parseJson=False, JsonBody=False)
+        return hashlib.md5(ret.content).digest().hex()
+
+    def autoComplete(self, address, dbpath):
         collectList = self.getCollectorList()
         print(collectList)
         for item in collectList:
+            # if item["isHandled"] == True:continue
             detail = self.getCollectorDetail(item["wid"])
             form = self.getCollectorFormFiled(
                 detail["collector"]["formWid"], detail["collector"]["wid"])
-            self.autoFill(form)
-            # you can edit this form content by your self.
-            # autoFill can fill the form with default value.
-            self.submitCollectorForm(detail["collector"]["formWid"], detail["collector"]
-                            ["wid"], detail["collector"]["schoolTaskWid"], form, address)
+
+            formpath = "{dbpath}/{charac}.json".format(
+                charac=self.getFormCharac(item), dbpath=dbpath)
+            if os.path.exists(formpath):
+                with open(formpath, "rb") as file:
+                    def find(l, key_valueList: list):
+                        for item in l:
+                            b = True
+                            for k_v in key_valueList:
+                                if item[k_v[0]] != k_v[1]:
+                                    b = False
+                            if b:
+                                return item
+                        return None
+
+                    newForm = form
+                    form = json.loads(file.read().decode("utf-8"))
+                    for item in newForm:
+                        l = find(form, [['title', item['title']], [
+                                 'description', item['description']]])
+                        item['value'] = l['value']
+                        for fieldItemsList in item['fieldItems']:
+                            field = find(l['fieldItems'], [
+                                ['content', fieldItemsList['content']]])
+                            fieldItemsList['isSelected'] = field['isSelected']
+                    form = newForm
+                    self.autoFill(form)
+
+                self.submitCollectorForm(detail["collector"]["formWid"], detail["collector"]
+                                         ["wid"], detail["collector"]["schoolTaskWid"], form, address)
+            else:
+                with open(formpath, "wb") as file:
+                    file.write(json.dumps(
+                        form, ensure_ascii=False).encode("utf-8"))
+                    print("请手动填写{formpath}，之后重新运行脚本".format(formpath=formpath))
+                    exit()
 
         confirmList = self.getNoticeList()
         print(confirmList)
         for item in confirmList:
             self.confirmNotice(item["noticeWid"])
 
-def main(argv):
-    try:
-        opts, args = getopt.getopt(argv, "h", [])
-        for opt, arg in opts:
-            if opt == '-h':
-                print("useage:\nDailyCP.py <school> <account> <password> <location>\n\n<school>: the address prefix of your school, if the address of your school is \"sise.campusphere.net\", then you can input sise.\n<account>: the login account of your school, usually is your student number.\n<password>: your login password.\n<location>: e.g.:中国广东省佛山市禅城区福贤路X号")
-                sys.exit()
-        if len(args) != 4:
-            print('useage: DailyCP.py <school> <account> <password> <location>')
-            sys.exit()
-    except getopt.GetoptError:
-        print('useage: DailyCP.py <school> <account> <password> <location>')
-        sys.exit()
-    else:
-        school = str(args[0])
-        account = str(args[1])
-        password = str(args[2])
-        address = str(args[3])
-    # print("Login information：")
-    # print("Account：" + account)
-    # print("Password：" + password)
-    # print("Location：" + address)
-    # sys.exit()
-    app = DailyCP(school)
-    flag = 0
-    while True:
-        # account = input("请输入帐号：")
-        # cap = ""
-        # if app.checkNeedCaptcha(account):
-        #     with io.open("Captcha.png", "wb") as file:
-        #         file.write(app.generateCaptcha())
-        #     cap = input("请输入验证码(Captcha.png)：")
-        # password = input("请输入密码：")
-        
-        ret = app.login(account, password)
-        time.sleep(3)
-        if ret:
-            print("Login succeeded.")
-            break
-        else:
-            flag = flag + 1
-            print("Login failed. Number of login attempts:" + str(flag))
-        if flag >= 10:
-            print("Can't login, exit.")
-            sys.exit()
-    # address = input("请输入定位地址：")  # "C-137平行宇宙，地球，中国" 中国广东省佛山市禅城区福贤路
-    app.autoComplete(address)
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    if len(sys.argv) != 6:
+        print("python3 DailyCp.py 学校全名 学号 密码 定位地址 formdb文件夹绝对路径")
+        exit()
+    app = DailyCP(sys.argv[1])
+    if not app.login(sys.argv[2], sys.argv[3]):
+        exit()
+    app.autoComplete(sys.argv[4], sys.argv[5])
 
-# Usage:
-#   just edit your id and password.
-#   if you are not auster,you need replace the host
-#   and run this script
 # Author:HuangXu,FengXinYang,ZhouYuYang.
 # By:AUST HACKER
 
-#2020/5/20 重要更新：修复登录过程，移除验证码（不需要），优化代码格式，感谢giteee及时反馈。
+# 2020/5/20 重要更新：修复登录过程，移除验证码（不需要），优化代码格式，感谢giteee及时反馈。
+# 2020/5/28 更改为使用自动获取学校URL的方式，更改为使用参数形式，添加另一种登录形式AuthServer的支持(已完成但未测试)。感谢柠火的反馈。
+# 2020/6/1 修复BUG，发现AuthServer的登录方式每个学校都不一样。支持任意表单内容自定义（详情见输出信息和formdb/1129.json）。感谢涅灵的反馈。
+# 2020/6/2 AuthServer的登录网址不再使用硬编码的方式，理论上能支持所有学校了吧？感谢涅灵的反馈。
+# 2020/6/17 修复crontab使用中相对路径的问题。识别form特征。
+# 2020/7/5 浪费别人的时间是一种可耻的行为。
--- README.md
+++ README.md
@@ -1,38 +1,48 @@
-# DailyCP
-## 简介
-今日校园自动打卡脚本。
-
-## 使用说明
+# FuckDailyCP
+#### 简介
+今日校园自动打卡脚本（不局限于健康打卡，包括收集表，通知确认）。
+可以挂服务器上定时自动打卡。
+健康的人可以用一下（雾）。
+![](doc/img.png)
+#### 使用说明
+理论上支持大部分学校（IAP登录方式，自己先试试能用不）和任意表单内容的自定义。目前已知两种学工号登录方式，均已实现。
+运行以下命令即可
 ```bash
-pip3 install -U -r requirements.txt
-python3 DailyCP.py <学校前缀> <账号> <密码> <定位地址>
+pip install -r requirements.txt
+python3 DailyCP.py 学校全名 学号 密码 定位地址 formdb绝对路径
 ```
-*提示No module named 'setuptools'时请先手动运行`pip3 install setuptools`*<br>
-*学校前缀可以在[这里](https://static.campushoy.com/apicache/tenantListSort)自行查询*
-## 用栗
+#### 关于自定义任意表单内容
+因为表单的内容多种多样，也不可能共享给其他人使用，因此本脚本采用“一次编辑，永久使用”的思路。
+如果脚本发现未知的表单，会将表单保存到formdb文件夹下，用户必须手动填写好内容，再次运行脚本即可。
+关于如何填写，参考example（1129.json）
+#### 关于AuthServer的登录方式
+目前已知IAP和AuthServer这两种登录方式，IAP是统一的，AuthServer每个学校都不一样。
+如果你发现脚本并不能正常运作，请根据输出的信息自行修改代码。
+#### 挂服务器提示
+请注意系统时间是否设置正确
+
+Linux下可以使用`corntab -e`设置定时任务让程序每天自动打卡
 ```bash
-python3 DailyCP.py sise 1640915111 password 中国广东省佛山市禅城区福贤路
+0 9 * * * python3 DailyCP.py 学校全名 学号 密码 定位地址
 ```
-## 自动签到
+我已经挂服务器四个月惹，稳的不行。
+#### 使用GITHUB ACTIONS自动签到
+去GITHUB新建一个仓库，将本项目的.github文件夹上传至你的仓库，点击Settings，增加`SCHOOL_NAME`、`ACCOUNT`、`PASSWORD`、`ADDRESS` 4个Secrets，分别对应你的学校名称、账号、密码、定位地址。<br>
+![Secrets](doc/Secrets.png)
 
-### 计划任务
-*请注意系统时间是否设置正确*<br>
-可以使用`corntab -e`设置定时任务`0 9 * * * /usr/bin/python3 <学校前缀> <账号> <密码> <定位地址>`
-##### 用栗
-```bash
-0 9 * * * /usr/bin/python3 /home/pi/DailyCP/DailyCP.py sise 1640915111 password 中国广东省佛山市禅城区福贤路
-```
-### GitHub ACTIONS 自动签到
-Fork本项目，在你的仓库下点击Settings，增加`SCHOOL_NAME`、`ACCOUNT`、`PASSWORD`、`ADDRESS` 4个Secrets，分别对应上述的`<学校前缀> <账号> <密码> <定位地址>`，之后打开你仓库的ACTIONS，最后手动提交一次代码修改就能激活自动签到了。<br>
-![Secrets](https://github.com/lemofire/DailyCP/blob/master/doc/Secrets.png)
+##### ACTIONS运行方式：
+* 1.push
+* 2.自己给自己star
+* 3.每日9点自动运行
 
-##### 签到方式：
+第一次使用需要根据Actions的`Run program`中的提示修改`.github/workflows/AutoDailyCP.yml`文件中的`cat>formdb/36.json<<EOF`。<br>
+~~当然你也可以使用[旧版](https://github.com/lemofire/DailyCP)就不用修改了~~<br>
+栗子：<br>
+* 提示：`请手动填写./formdb/41.json，之后重新运行脚本`<br>
+* 修改后：`cat>formdb/41.json<<EOF`<br>
+每次运行action后会自动打包formdb，可以根据里面的内容修改`cat>formdb/36.json<<EOF`下的代码
 
-###### 手动签到
-* push
-* 自己给自己star
-###### 自动签到
-* 每3小时自动签到一次
-
-## Credits
-* [Finch/FuckDailyCP](https://gitee.com/Finch1/FuckDailyCP)
+#### 效果截图
+![](doc/psc.jpg)
+#### 最后
+小老板，给个Star?
